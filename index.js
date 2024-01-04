0c2b403c94b0842c69abe58d39247fa5487c73de
shubham ralli <sralli73@gmail.com>
2024-01-04 09:34:56 +0530

    p

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000; // Use the environment port or default to 3000
+const port = process.env.PORT || 3000;
 const downloadController = require('./downloadController');
 
 app.use(downloadController);
